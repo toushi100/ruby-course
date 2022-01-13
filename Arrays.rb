97ba07ae3996b776dbc010ac8f19054a03b11899
toushi100 <toushi2009@gmail.com>
2022-01-13 05:31:20 -0500

    added arrays file

---
+++ Arrays.rb
@@ -0,0 +1,4 @@
+a = ["Ahmed", "Mady",  "x","Abdo","Rawan"]
+print a.delete_if {|x| x.length>1 }
+a = [1+2]*10
+print a
--- Strings.rb
+++ Strings.rb
@@ -1,5 +1,20 @@
-x = "Ruby Rocks!"
-x["Rocks!"] = ""
-x+="is wonderful"
-puts x
+# x = "Ruby Rocks!"
+# x["Rocks!"] = ""
+# x+="is wonderful"
+# puts x
+
+
+# r = "11Ahmez!"
+# x = [1,2,3,4,5,6,7,8]
+# t = [2,3]
+
+# # puts t = r[0,5].upcase()
+# # puts t = r[0,5].downcase()
+# # puts t.hash()
+# # puts t.upcase().hash()
+# # puts t.downcase().hash()
+# # puts t.reverse().hash()
+# print x[0..5]
+# print x[0,5]
+
 
