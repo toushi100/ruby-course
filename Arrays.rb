919dab48f52b97d9bbe2b3ad2ec359fe2ec00ed0
toushi100 <toushi2009@gmail.com>
2022-01-13 05:54:46 -0500

    added hashes file

--- Arrays.rb
+++ Arrays.rb
@@ -1,4 +1,11 @@
-a = ["Ahmed", "Mady",  "x","Abdo","Rawan"]
-print a.delete_if {|x| x.length>1 }
-a = [1+2]*10
+# a = ["Ahmed", "Mady",  "x","Abdo","Rawan"]
+# print a.delete_if {|x| x.length>1 }
+# a = [1+2]*10
+# print a
+
+a = [1,2,3,4,5,6,7,8,9]
+a.append(2,3,4,5,99)
+print a
+puts
+a.push(2,43,54,65,76,86)
 print a
---
+++ Hashes.rb
@@ -0,0 +1,8 @@
+skill = {
+    "ruby"=>12,
+    rub:2,
+    "dub":3
+}
+puts skill['rubdddy'] = true
+print skill["ruby"]
+print skill["dub"]
